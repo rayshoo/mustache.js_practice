d13aa9c057cff4b9d7434f0f3bca3d7a8f15ca1a
dfnk5516 <rayshoo@naver.com>
2020-05-19 02:09:15 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.set('views', path.join(__dirname, './views'))
+app.set('view engine', 'ejs');
+
+app.use(express.json());
+app.use(express.urlencoded({extended : false}));
+app.use('/', express.static(path.join(__dirname, './public')));
+
+app.get('/', (req, res, next)=>{
+  res.render('index');
+})
+
+app.listen(3000, ()=>{
+  console.log('server started...');
+})
